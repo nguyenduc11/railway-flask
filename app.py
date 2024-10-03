55589e1674b0eb8e8e1cae50a03b60363e5afd4a
nguyenduc <nguyenduc11@gmail.com>
2024-10-03 14:02:41 +0700

    commit

---
+++ app.py
@@ -0,0 +1,47 @@
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form['title']
+    description = request.form['description']
+    new_task = Task(title=title, description=description)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Task.query.get_or_404(id)
+    if request.method == 'POST':
+        task.title = request.form['title']
+        task.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update.html', task=task)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
